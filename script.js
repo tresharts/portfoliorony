e85813bfada9625185693e7c4455a2681c45c6b9
Arthur <arthurbruno489@gmail.com>
2026-02-01 00:38:26 -0300

    Estrutura inicial

---
+++ script.js
@@ -0,0 +1,16 @@
+function abrirModal(listaDeImagens){
+    conteudoModal.innerHTML = "";
+    listaDeImagens.forEach(link => {
+        conteudoModal.innerHTML += `<img src="${link}">`;
+    });
+    conteudoModal.scrollTop = 0;
+    overlay.classList.add("active");
+    modal.classList.add("active");
+    navbar.classList.add("hidden");
+}
+
+function fecharModal(){
+    overlay.classList.remove("active");
+    modal.classList.remove("active");
+    navbar.classList.remove("hidden");
+}
